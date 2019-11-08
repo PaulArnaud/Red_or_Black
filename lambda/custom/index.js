cbe326dca6d73663a13b1f39a9a0af15c3148d17
Nyandams <lecha.damien@gmail.com>
2019-11-08 16:26:29 +0100

    redIntent bug

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -9,16 +9,42 @@ const i18n = require('i18next');
 // i18n strings for all supported locales
 const languageStrings = require('./languageStrings');
 
-const colorIntents = ["RedIntent", "BlackIntent"];
-const compareIntents = ["HigherIntent", "BelowIntent"]
-const interExterIntents = ["InternIntent", "ExternIntent"]
-const suiteIntents = ["DiamondsIntent", "ClubsIntent", "SpadesIntent", "HeartsIntent"]
+const {decks} = require('cards');
 
-const WELCOME_MESSAGE =  "Welcome in Red or Black! Please play moderatly to this game. How many player do you want"
-const NB_PLAYER_ERROR_MESSAGE = "The number of players has already been set"
-const NB_PLAYER_WRONG_ERROR_MESSAGE = "Please set a number a players between 1 and 13"
+const colorIntents = ["RedIntent", "BlackIntent"];
+const compareIntents = ["HigherIntent", "BelowIntent"];
+const interExterIntents = ["InternIntent", "ExternIntent"];
+const suiteIntents = ["DiamondsIntent", "ClubsIntent", "SpadesIntent", "HeartsIntent"];
+
+const WELCOME_MESSAGE =  "Welcome in Red or Black! Please play moderatly to this game. How many player do you want";
+const NB_PLAYER_ERROR_MESSAGE = "The number of players has already been set";
+const NB_PLAYER_WRONG_ERROR_MESSAGE = "Please set a number of players between 1 and 13";
+const NOT_ENOUGH_PLAYER_ERROR_MESSAGE = "I asked you to give me the name of the next player !";
+
+const PHASE_ERROR_MESSAGE = [
+    "I asked you, RED OR BLACK !",
+    "I asked you, HIGHER or BELOW !",
+    "I asked you, IN or OUT !",
+    "I asked you, A SUITE !"
+]
 
 const ADD_PLAYER_ERROR_MESSAGE = "You cannot add another player in the game"
+
+const cardValue = new Map([
+    ["Ace", 1],
+    ["Two", 2],
+    ["Three", 3],
+    ["Four", 4],
+    ["Five", 5],
+    ["Six", 6],
+    ["Seven", 7],
+    ["Eight", 8],
+    ["Nine", 9],
+    ["Ten", 10],
+    ["Jack", 11],
+    ["Queen", 12],
+    ["King", 13]
+  ]);
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -155,7 +181,7 @@ const LocalisationRequestInterceptor = {
 
 
 /* OUR WORK */
-const SetNbPlayerHandler = {
+const SetNbPlayerIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SetNbPlayerIntent';
@@ -171,10 +197,14 @@ const SetNbPlayerHandler = {
                 .getResponse();
         }
 
+        const deck = new decks.StandardDeck();
+        deck.shuffleAll();
         const sentence = "The game is launched with " + nbPlayers + " players, What\'s the name of the player one ?";
         sessionAttributes.numberOfPlayer = nbPlayers;
         sessionAttributes.playersList = [];
         sessionAttributes.currentPlayer = 1;
+        sessionAttributes.deck = deck
+        console.log("the deck:" + deck)
         handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
         return handlerInput.responseBuilder
             .speak(sentence)
@@ -222,13 +252,69 @@ const SetNamePlayerIntentHandler = {
     }
 };
 
+const RedIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RedIntent';
+    },
+    handle(handlerInput) {
+        const  sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        let {verif, errorMessage} = verifIntent(sessionAttributes, Alexa.getIntentName(handlerInput.requestEnvelope))
+        if(!verif){
+            return handlerInput.responseBuilder
+                .speak(errorMessage)
+                .reprompt()
+                .getResponse();
+        }
+
+        const cardDraw = sessionAttributes.deck.draw(1)[0]
+        const suit = cardDraw.suit.name
+        const rank = cardDraw.rank.longName
+        console.log("salut à tous" + cardDraw)
+        var sentence = "You draw a " + cardValue.get(rank) + " of " + suit;
+        /*
+        if (suit == "hearts" || suit == "diamonds"){
+            sentence = sentence + ", you won !"
+        } else {
+            sentence = sentence + ", you lost !"
+        }
+
+        sessionAttributes.playersList[sessionAttributes.currentPlayer].cards.push(cardDraw)
+        sessionAttributes.currentPlayer = sessionAttributes.currentPlayer + 1;
+        if (sessionAttributes.currentPlayer == sessionAttributes.playersList.length){
+            sessionAttributes.currentPlayer = 0
+            sentence = sentence + sessionAttributes.playersList[sessionAttributes.currentPlayer].name + "! IN or OUT?";
+        }*/
+        
+        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+        return handlerInput.responseBuilder
+            .speak(sentence)
+            .reprompt("What\'s the player name ?")
+            .getResponse();
+
+    }
+};
+
+
 /*
 Be careful, you have to order 
 */
 
 function verifIntent(session, intent, parameter = undefined) {
+    const checkInPhase = () => {
+        return (session.numberOfPlayer && session.playersList.length == session.numberOfPlayer)
+    }
+
     if (colorIntents.includes(intent)){
-        return {verif: true, error: ""}
+        if (checkInPhase && session.playersList[session.currentPlayer].cards.length == 0){
+            return {verif: true, error: ""}
+        } else if (!session.numberOfPlayer){
+            return {verif: false, errorMessage: NB_PLAYER_WRONG_ERROR_MESSAGE}
+        } else if(session.numberOfPlayer && session.playersList.length < session.numberOfPlayer){
+            return {verif: false, errorMessage: NOT_ENOUGH_PLAYER_ERROR_MESSAGE}
+        } else {
+            return {verif: false, errorMessage: PHASE_ERROR_MESSAGE}
+        }
     } else if (compareIntents.includes(intent)) {
         return {verif: true, errorMessage: ""}
     } else if (interExterIntents.includes(intent)){
@@ -259,8 +345,9 @@ function verifIntent(session, intent, parameter = undefined) {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        SetNbPlayerHandler,
+        SetNbPlayerIntentHandler,
         SetNamePlayerIntentHandler,
+        RedIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         FallbackIntentHandler,
